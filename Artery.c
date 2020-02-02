e77a0c15e7dbc043a65a7f4a16df2be90eac4954
Tim Gremalm <tim@gremalm.se>
2020-02-02 06:46:01 +0100

    Swipe repeating pattern

--- Artery.c
+++ Artery.c
@@ -79,18 +79,88 @@ void e131task(void *pvParameters) {
 	}
 }
 
+typedef struct {
+	uint8_t pulse_width;
+	uint8_t pulse_gap;
+	uint8_t pulse_bpm;
+	uint8_t pulse_flow;
+	float pulse_hue;
+	float pulse_light_min;
+	float pulse_light_max;
+} beat_parameters_t;
+
+enum DMX_CHANNEL {
+	DMX_WIDTH,
+	DMX_GAP,
+	DMX_BPM,
+	DMX_FLOW,
+	DMX_HUE,
+	DMX_LIGHT_MIN,
+	DMX_LIGHT_MAX,
+};
+
+void init_dmx_values(int dmx_address) {
+	pwbuff->property_values[dmx_address+DMX_LIGHT_MAX] = 10;
+	pwbuff->property_values[dmx_address+DMX_LIGHT_MIN] = 10;
+	pwbuff->property_values[dmx_address+DMX_HUE] = 0;
+	pwbuff->property_values[dmx_address+DMX_WIDTH] = 2;
+	pwbuff->property_values[dmx_address+DMX_GAP] = 4;
+	pwbuff->property_values[dmx_address+DMX_BPM] = 128;
+	pwbuff->property_values[dmx_address+DMX_FLOW] = 1;
+}
+
+beat_parameters_t readparametersfromdmx(int dmx_address) {
+	beat_parameters_t paramout;
+	// Read parameters from DMX values
+	paramout.pulse_light_max = (float)pwbuff->property_values[dmx_address+DMX_LIGHT_MAX] / 255;
+	paramout.pulse_light_min = ((float)pwbuff->property_values[dmx_address+DMX_LIGHT_MIN] / 255) * paramout.pulse_light_max;
+	paramout.pulse_hue = (float)pwbuff->property_values[dmx_address+DMX_HUE] / 255;
+	paramout.pulse_width = pwbuff->property_values[dmx_address+DMX_WIDTH];
+	paramout.pulse_gap = pwbuff->property_values[dmx_address+DMX_GAP];
+	paramout.pulse_bpm = pwbuff->property_values[dmx_address+DMX_BPM];
+	paramout.pulse_flow = pwbuff->property_values[dmx_address+DMX_FLOW];
+
+	// Make sure values are whithin ranges
+	if (paramout.pulse_width < 1) {
+		paramout.pulse_width = 1;
+	}
+	if (paramout.pulse_gap < 1) {
+		paramout.pulse_gap = 1;
+	}
+	if (paramout.pulse_bpm < 1) {
+		paramout.pulse_bpm = 1;
+	}
+
+	return paramout;
+}
+
 void lighttask(void *pvParameters) {
 	//NodeMCU D9 GPIO3 I2S https://github.com/nodemcu/nodemcu-devkit-v1.0#pin-map
-	uint32_t led_number = 12;
+	uint32_t led_number = 110;
+	int dmx_address = 1;
+	init_dmx_values(dmx_address);
 	ws2812_pixel_t pixels[led_number];
 	ws2812_i2s_init(led_number, PIXEL_RGB);
 	memset(pixels, 0, sizeof(ws2812_pixel_t) * led_number);
+	beat_parameters_t beat_parameters;
+	int segment_pixels;
+	int segment_index;
+	float segment_offset = 0.0;
 
 	while(1) {
-		uint8_t lightness = pwbuff->property_values[1];
-		printf("Channel 1: %d\n", lightness);
+		beat_parameters = readparametersfromdmx(dmx_address);
+		segment_pixels = beat_parameters.pulse_width + beat_parameters.pulse_gap;
+		segment_offset += (float)beat_parameters.pulse_flow / 100;
+		if (segment_offset > segment_pixels || segment_offset < -segment_pixels) {
+			segment_offset = 0;
+		}
 		for (int i = 0; i < led_number; i++) {
-			pixels[i] = hslToRgb(0, 1.0, (float)lightness/255);
+			segment_index = (abs(i-(int)segment_offset+segment_pixels)) % segment_pixels;
+			if (segment_index < beat_parameters.pulse_width) {
+				pixels[i] = hslToRgb(beat_parameters.pulse_hue, 1.0, beat_parameters.pulse_light_max);
+			} else {
+				pixels[i] = hslToRgb(beat_parameters.pulse_hue, 1.0, 0.0);
+			}
 		}
 		ws2812_i2s_update(pixels, PIXEL_RGB);
 		vTaskDelay(50 / portTICK_PERIOD_MS);
