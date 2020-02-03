0e9d0373e23141248e03ba8d75a1f2733f236698
Tim Gremalm <tim@gremalm.se>
2020-02-03 02:53:54 +0100

    Blink to beat

--- Artery.c
+++ Artery.c
@@ -148,15 +148,47 @@ void lighttask(void *pvParameters) {
 	float segment_offset = 0.0;
 	float pixel_lightlevel;
 	float pixel_rest;
+	float beat_level;
+	uint32_t beat_start = 0;
+	uint32_t beat_mid = 0;
+	uint32_t beat_end = 0;
+	uint32_t now;
+	uint32_t beatms;
+	uint32_t diff;
 
 	while(1) {
 		beat_parameters = readparametersfromdmx(dmx_address);
-		segment_pixels = beat_parameters.pulse_width + beat_parameters.pulse_gap;
+
+		// Calculate beat, and light level on beat
+		//printf("Tick %d\n", xTaskGetTickCount()*portTICK_PERIOD_MS);
+		now = xTaskGetTickCount()*portTICK_PERIOD_MS;
+		beatms = (((float)60/beat_parameters.pulse_bpm)*1000)/2;
+		if (now > beat_end) {
+			// Calculate next beat
+			beat_start = now;
+			beat_mid = now + beatms;
+			beat_end = beat_mid + beatms;
+			//printf("Double beat %d %d\n", beatms, beat_end);
+		}
+		if (now < beat_mid) {
+			// Start of beat, go up
+			diff = beat_mid - now;
+			beat_level = (float)diff / beatms;
+		} else {
+			// End of beat, go down
+			diff = beat_end - now;
+			beat_level = 1.0 - ((float)diff / beatms);
+		}
+		//printf("Beat level %f\n", beat_level);
+
 		// By offseting segments a movement can be created
+		segment_pixels = beat_parameters.pulse_width + beat_parameters.pulse_gap;
 		segment_offset += (float)beat_parameters.pulse_flow / 100;
 		if (segment_offset > segment_pixels || segment_offset < -segment_pixels) {
 			segment_offset = 0;
 		}
+
+		// Render
 		for (int i = 0; i < led_number; i++) {
 			segment_index = (abs(i-(int)segment_offset+segment_pixels)) % segment_pixels;
 			if (segment_index < beat_parameters.pulse_width) {
@@ -169,12 +201,14 @@ void lighttask(void *pvParameters) {
 				if (segment_index == beat_parameters.pulse_width-1) {
 					pixel_lightlevel = pixel_lightlevel * pixel_rest;
 				}
+				pixel_lightlevel = pixel_lightlevel * beat_level;
 			} else {
 				pixel_lightlevel = 0;
 			}
 			pixels[i] = hslToRgb(beat_parameters.pulse_hue, 1.0, pixel_lightlevel);
 		}
 		ws2812_i2s_update(pixels, PIXEL_RGB);
+
 		vTaskDelay(50 / portTICK_PERIOD_MS);
 	}
 }
